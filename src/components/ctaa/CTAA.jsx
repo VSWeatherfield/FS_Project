c8c22fcdd5b7614041909dec7cb89947a2af15c1
Vladimir <voff.smirnoff@gmail.com>
2022-10-04 21:13:27 +0600

    start

---
+++ src/components/ctaa/CTAA.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ctaa.css'
+
+const CTAA = () => {
+  return (
+    <div>CTAA</div>
+  )
+}
+
+export default CTAA
---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './feature.css'
+
+const Feature = () => {
+  return (
+    <div>feature.css</div>
+  )
+}
+
+export default Feature
