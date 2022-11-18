ce14178fb7622d5553d33d8899ca2ec9aec2ffd9
Vladimir Smirnov <voff.smirnoff@gmail.com>
2022-11-18 13:42:14 +0300

    Delete src directory

--- src/containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from "./header/Header";
-export { default as Footer } from "./footer/Footer";
-export { default as BlogItem } from "./blogItem/blogItem/Blog-item";
-export { default as AnswerItem } from "./blogItem/answerItem/AnswerItem";
-export { default as AnswerList } from "./blogItem/answersList/AnswerList";
--- src/services/ajaxservice.js
+++
@@ -1,5 +0,0 @@
-export function ajaxService(url, params) {
-  return fetch("http://localhost:5000" + url, params).then((data) => {
-    return data.json();
-  });
-}
