03ca70825a69eef5d1a9db72fdcba4831223a63b
Vladimir <voff.smirnoff@gmail.com>
2022-11-23 15:27:25 +0600

    user model redressal
    corrected the profile model, now it works according to one-to-one relationship

--- back/back/settings.py
+++ back/back/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'olymp',
     'year',
     'problem',
-    'user'
+    'userprofile'
 ]
 
 MIDDLEWARE = [
---
+++ back/userprofile/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+
+    location = models.CharField(max_length=30, blank=True)
+    num_views = models.IntegerField(default=0)
+
+    date_activity = models.DateTimeField(default=timezone.now)
+    last_answer = models.DateTimeField(default=timezone.now)
+    
+
+    def __str__(self) -> str:
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
